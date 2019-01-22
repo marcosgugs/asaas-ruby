f476770fcd5ee4b18dac6838797668821a6a3084
Marcos Junior <marcos.junior@rhsoftware.com.br>
2019-01-21 22:19:36 -0200

    Add result fields to payment model

--- lib/asaas/models/payment.rb
+++ lib/asaas/models/payment.rb
@@ -1,6 +1,7 @@
 module Asaas
   class Payment < Model
     BillingTypes = Types::Strict::String.enum('BOLETO', 'CREDIT_CARD', 'UNDEFINED')
+    Status = Types::Strict::String.enum('PENDING', 'RECEIVED', 'CONFIRMED', 'OVERDUE', 'REFUNDED', 'RECEIVED_IN_CASH', 'REFUND_REQUESTED', 'CHARGEBACK_DISPUTE', 'AWAITING_CHARGEBACK_REVERSAL')
 
     attribute :id, Types::Coercible::String.optional.default(nil)
     attribute :customer, Types::Coercible::String
@@ -14,5 +15,20 @@ module Asaas
     attribute :discount, Discount.optional.default(nil)
     attribute :interest, Interest.optional.default(nil)
     attribute :fine, Fine.optional.default(nil)
+
+    attribute :status, Status.optional.default(nil)
+    attribute :dateCreated, Types::Coercible::String.optional.default(nil)
+    attribute :subscription, Types::Coercible::String.optional.default(nil)
+    attribute :originalDueDate, Types::Coercible::String.optional.default(nil)
+    attribute :originalValue, Types::Coercible::Decimal.optional.default(nil)
+    attribute :interestValue, Types::Coercible::Decimal.optional.default(nil)
+    attribute :confirmedDate, Types::Coercible::String.optional.default(nil)
+    attribute :paymentDate, Types::Coercible::String.optional.default(nil)
+    attribute :clientPaymentDate, Types::Coercible::String.optional.default(nil)
+    attribute :invoiceUrl, Types::Coercible::String.optional.default(nil)
+    attribute :bankSlipUrl, Types::Coercible::String.optional.default(nil)
+    attribute :invoiceNumber, Types::Coercible::String.optional.default(nil)
+    attribute :deleted, Types::Coercible::String.optional.default(nil)
+
   end
 end
