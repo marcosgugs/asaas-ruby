c5c134f47997b5eccca67db95454f27adaa2003d
Thean Rogerio <thean.rogerio@rhsoftware.com.br>
2019-12-12 16:19:42 -0300

    Add 'DEPOSIT' to Payment BillingTypes

--- lib/asaas/models/payment.rb
+++ lib/asaas/models/payment.rb
@@ -1,6 +1,6 @@
 module Asaas
   class Payment < Model
-    BillingTypes = Types::Strict::String.enum('BOLETO', 'CREDIT_CARD', 'UNDEFINED', 'TRANSFER')
+    BillingTypes = Types::Strict::String.enum('BOLETO', 'CREDIT_CARD', 'UNDEFINED', 'TRANSFER', 'DEPOSIT')
     Status = Types::Strict::String.enum('PENDING', 'RECEIVED', 'CONFIRMED', 'OVERDUE', 'REFUNDED', 'RECEIVED_IN_CASH', 'REFUND_REQUESTED', 'CHARGEBACK_DISPUTE', 'AWAITING_CHARGEBACK_REVERSAL')
 
     attribute :id, Types::Coercible::String.optional.default(nil)
@@ -29,6 +29,5 @@ module Asaas
     attribute :bankSlipUrl, Types::Coercible::String.optional.default(nil)
     attribute :invoiceNumber, Types::Coercible::String.optional.default(nil)
     attribute :deleted, Types::Coercible::String.optional.default(nil)
-
   end
 end
